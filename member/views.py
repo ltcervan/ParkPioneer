4a526bad3cc02d122aab27faab43bf8126ca338f
ltcervan <ltcervan@gmail.com>
2024-02-19 00:42:38 -0800

    FEAT: Complete path for member/login with functioning template

--- member/views.py
+++ member/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.template import loader
 from .forms import UserRegisterForm
 
 def register(request):
@@ -15,6 +16,20 @@ def register(request):
             return redirect('home')
     else:
         form = UserRegisterForm()
-    return render(request, 'member/register.html', {'form': form})
+    return render(request, 'authentication/register.html', {'form': form})
 
-
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                form.add_error(None, "Invalid username or password")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
