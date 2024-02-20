553e56250e82d4ff4ed563e75fd0e1d7a0e7f8dc
ltcervan <ltcervan@gmail.com>
2024-02-20 00:14:04 -0800

    FEAT: created itinerary model

--- itinerary/models.py
+++ itinerary/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class Itinerary(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='itineraries')
+    park_title = models.CharField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    event = models.TextField()
+
+    def __str__(self):
+        return f"{self.park_title} from {self.start_date} to {self.end_date}"
+
+    def is_upcoming(self):
+        return self.start_date > timezone.now().date()
+
+    def is_past(self):
+        return self.end_date < timezone.now().date()
+
+
