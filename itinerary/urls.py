164b24700453e35b8a203493526a76d7df10b92a
ltcervan <ltcervan@gmail.com>
2024-02-21 12:28:22 -0800

    FEAT: main page set up

--- itinerary/urls.py
+++ itinerary/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import main_view  # Replace with your actual view that renders main.html
+
 
 urlpatterns = [
+    path('main/', main_view, name='main'),
     path('itineraries/', views.itinerary_list, name='itinerary_list')
 ]
